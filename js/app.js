d945a72c64d84aa6ccc0bedd583bf797450eabe5
Luke Schuyler <lukeschuyler1@gmail.com>
2017-02-28 00:09:49 -0600

    ordered archive

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ bandmates.run(function($ionicPlatform) {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
